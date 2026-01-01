c6ded50dd54298fa28adf2cf924c570d439d7ca6
Zhengan Chen <zhenganc@kth.se>
2026-01-01 07:31:44 +0100

    primary PPO @50M

--- algorithms/SB3_DQN.py
+++ algorithms/SB3_DQN.py
@@ -1,9 +1,15 @@
 from envs import tetris_env
 from stable_baselines3 import DQN
+import torch
+import gymnasium as gym
 
-log_dir = "logs"
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+print("Device: ", device)
 
-env = tetris_env.make_env(False)
+log_dir = "test"
+
+env = gym.make("tetris_gymnasium/Tetris")
+# env = tetris_env.make_env(False)
 # print(env.observation_space)
 model = DQN(
     "MultiInputPolicy",
@@ -12,6 +18,7 @@ model = DQN(
     tensorboard_log=log_dir,
     learning_rate=0.00025,
     exploration_fraction=0.2,
+    # device=device,
 )
 # model = DQN("MultiInputPolicy", env, verbose=1)
 model.learn(total_timesteps=50_000_000, log_interval=1000, tb_log_name="DQN_50M_dict")
--- algorithms/SB3_PPO_con.py
+++ algorithms/SB3_PPO_con.py
@@ -9,10 +9,53 @@ model = PPO.load("models_con/PPO_8_5_2_1_1", vec_env)
 # (alife=0.05, game_over=-10)
 # PPO{obs_size}_{T-steps}_{d_holes==0 -> +0.a}_{-0.b * d_holes} _{clear_line^c * 1 + 1}_{-0.d * d_height}
 model.learn(
-    total_timesteps=5_000_000,
+    total_timesteps=40_000_000,
     tb_log_name="PPO_8_5_2_1_1",
     reset_num_timesteps=False,
 )
 model.save("models_con/PPO_8_5_2_1_1")
 
 # uv run python -m algorithms.SB3_PPO_con
+
+
+# def step(self, action):
+#     obs, reward, term, trun, info = self.env.step(action)
+
+#     if reward > 1:
+#         reward = 0.01
+
+#     lines_cleared = info.get("lines_cleared", 0)
+#     if lines_cleared > 0:
+#         reward += (lines_cleared**2) * 1 + 2
+
+#     max_height = obs[10]
+#     holes = obs[11]
+#     bumpiness = obs[12]
+
+#     # when the block is placed
+#     if reward > 0:
+#         # delta (have all been divided by 20 in the observation function)
+#         d_holes = holes - self.prev_holes
+#         d_bump = bumpiness - self.prev_bumpiness
+#         d_height = max_height - self.prev_max_height
+
+#         # reward += -0.001 * max_height - 0.005 * holes - 0.001 * bumpiness
+#         reward += -0.5 * d_holes - 0.01 * d_height - 0.01 * d_bump
+#         # self.prev_max_height = max_height
+#         # self.prev_bumpiness = bumpiness
+#         self.prev_holes = holes
+#         if d_holes == 0:
+#             reward += 0.8
+#         # print("0 delta hole")
+
+#         # print("delta hole: ", d_holes)
+#         self.env.prev_state = obs[:10]
+
+#         if self.env.obs_size in (26, 32):
+#             obs[-10:] = self.env.prev_state
+
+#     if self.env.obs_size not in (26, 32):
+#         obs[:10] = self.env.prev_state
+
+#     if action == 6:  # swap
+#         reward -= 0.05
