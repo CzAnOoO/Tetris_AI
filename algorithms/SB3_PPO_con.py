0b63e8d75c57253a08ed8da85b1bab9b2f73fb4b
Zhengan Chen <zhenganc@kth.se>
2026-01-01 20:05:32 +0100

    quick fix

--- algorithms/SB3_PPO_con.py
+++ algorithms/SB3_PPO_con.py
@@ -3,17 +3,17 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
 # Parallel environments
-vec_env = make_vec_env(lambda: tetris_env.make_env(render=False, obs_size=16), n_envs=8)
+vec_env = make_vec_env(lambda: tetris_env.make_env(render=False, obs_size=22), n_envs=8)
 
-model = PPO.load("models_con/PPO_8_5_2_1_1", vec_env)
+model = PPO.load("models_con/PPO22_8_5_2_1_3", vec_env)
 # (alife=0.05, game_over=-10)
 # PPO{obs_size}_{T-steps}_{d_holes==0 -> +0.a}_{-0.b * d_holes} _{clear_line^c * 1 + 1}_{-0.d * d_height}_{-0.0e * d_bump}
 model.learn(
     total_timesteps=50_000_000,
-    tb_log_name="PPO_8_5_2_1_3_fork",
+    tb_log_name="PPO22_8_5_2_1_3",
     reset_num_timesteps=False,
 )
-model.save("models_con/PPO_8_5_2_1_3_fork")
+model.save("models_con/PPO22_8_5_2_1_3")
 
 # uv run python -m algorithms.SB3_PPO_con
 
