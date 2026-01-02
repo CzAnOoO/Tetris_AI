67507f345d74e93fde99b9ec4c3027a49003f487
Zhengan Chen <zhenganc@kth.se>
2026-01-02 06:20:10 +0100

    PPO22_8_5_2_1_3 @150M

--- algorithms/SB3_PPO.py
+++ algorithms/SB3_PPO.py
@@ -2,7 +2,7 @@ from envs import tetris_env
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
-log_dir = "continuation/log"
+log_dir = "logs_con"
 # Parallel environments
 vec_env = make_vec_env(lambda: tetris_env.make_env(render=False, obs_size=22), n_envs=8)
 
@@ -11,8 +11,9 @@ model = PPO(
     vec_env,
     verbose=1,
     tensorboard_log=log_dir,
+    ent_coef=0.01,
 )
 # (alife=0.05, game_over=-10)
 # PPO{obs_size}_{T-steps}_{d_holes==0 -> +0.a}_{-0.b * d_holes} _{clear_line^c * 1 + 1}_{-0.0d * d_height}_{-0.0e * d_bump}_{-0.0f * max_height}
-model.learn(total_timesteps=50_000_000, tb_log_name="PPO22_3_6_2_0_3_2")
-model.save("continuation/model/PPO22_3_6_2_0_3_2")
+model.learn(total_timesteps=50_000_000, tb_log_name="PPO22_8_5_2_1_3_coef1")
+model.save("models_con/PPO22_8_5_2_1_3_coef1")
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from stable_baselines3 import DQN
 from stable_baselines3 import PPO
 
 if __name__ == "__main__":
-    env = tetris_env.make_env(True, obs_size=22)
+    env = tetris_env.make_env(True, obs_size=22, seed=42)
     # model = DQN.load("models/dqn_my_plz")
     # PPO16_10M_01_5_2_1
-    model = PPO.load("models_con/PPO22_8_5_2_0_5_1")
+    model = PPO.load("models_con/PPO22_8_5_2_1_3")
 
-    tetris_env.play(env, agent=model, delay=1, episodes=10, render=True)
+    tetris_env.play(env, agent=model, delay=50, episodes=1, render=True)
